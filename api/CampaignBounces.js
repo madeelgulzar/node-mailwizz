4bbe2a91894ef8192ebd70e70a395845596fe523
Adeel Gulzar <adeel.gulzar@eplanetcom.com>
2021-01-12 22:37:37 +0500

    Campaign Bounces API

---
+++ api/CampaignBounces.js
@@ -0,0 +1,22 @@
+const Request = require('./Request');
+
+const path = '/campaigns/';
+
+class Campaigns extends Request {
+    constructor(config) {
+        super(config);
+    }
+
+    getLists(campaignUid, page, perPage) {
+        this.method = Request.Type.GET;
+        this.url = path + campaignUid + '/bounces';
+        this.data = {
+            page: page,
+            per_page: perPage
+        };
+
+        return this.send();
+    }
+}
+
+module.exports = Campaigns;
